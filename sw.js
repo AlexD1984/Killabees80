d73b8765fc0beab1d554cff14357121a234d370e
AlexD1984 <dreher.alexander@gmx.de>
2025-06-08 16:35:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = 'killabees80-v3';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon.png',
+  './controller-icon.png',
+  './banner.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
